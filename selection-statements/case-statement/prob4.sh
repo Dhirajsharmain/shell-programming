3d530eec79d43ee6f8a0d30d65791b84fa7806e9
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-05-10 17:38:29 +0530

    changes in case statement

--- selection-statements/case-statement/prob4.sh
+++ selection-statements/case-statement/prob4.sh
@@ -3,8 +3,8 @@
 #Date of Creation : Sun, May  9, 2021  8:21:57 PM
 #Created By : Dhiraj
 echo "  1. Feet to Inch 
-        3. Inch to Feet
-        2. Feet to Meter 
+        2. Inch to Feet
+        3. Feet to Meter 
         4. Meter to Feet"
             
 read -p "Select any option : " num
@@ -15,20 +15,23 @@ then
     case $num in
 		1)
 			val=12
+			result=$(($Num*$val))
 		;;
 		2)
-			val=0.083333
+			val=12
+			result=$(($Num/$val))
 		;;
 		3)
-			val=3.24084
+			val=0.3048
+			result=$(($Num*$val))
 		;;
 		4)
-			val=3.24084
+			val=0.3048s
+			result=$(($Num/$val))
 		;;
 	esac
 else
     echo "Invalid Selection"
 fi
     
-    result=$(($Num*$val))
     echo Answer is : $result
