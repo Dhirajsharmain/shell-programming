10cb023e47161ecf861ef2d9654106c0272bb35d
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-05-19 09:55:55 +0530

    Update prob4.sh

--- repetition-statement/while-loop/prob4.sh
+++ repetition-statement/while-loop/prob4.sh
@@ -3,16 +3,16 @@
 #Date of Creation : Wed, May 12, 2021  1:23:01 PM
 #Created By : Dhiraj
 
-rs=100
+rs=50
 count=0
 wonCount=0
 loseCount=0
-while [[ $rs -le 200 || $rs -le 0 ]]
+while [[ $rs -le 100 && $rs -ge 0 ]]
 do
     randomNum=$((RANDOM%2+1))
     case $randomNum in
         1)
-            rs=$(($rs+2))
+            rs=$(($rs+1))
             wonCount=$(($wonCount+1))
         ;;
         2)
